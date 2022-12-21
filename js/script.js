2f6f970f6faa3ef3df15e4710ed3a27ef1af824a
RiccardoNestola <riccardinux@gmail.com>
2022-12-21 16:35:54 +0100

    add api random mail

--- js/script.js
+++ js/script.js
@@ -2,13 +2,48 @@ const { createApp } = Vue
 
     createApp({
         data() {
-        return {
+            return {
+
+            randomMail: null,
             
         }
     },
         methods: {
+
+        getRandomMail(){
+                
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                .then(function (response) {
+                this.randomMail = response.data.response
+                
+                console.log(this.randomMail);
+
+                })
+                .catch(function (error) {
+                
+                console.log(error);
+                })
+                .then(function () {
+                
+                });
+
+            }
             
+        },
+
+
+        created() {
+            this.getRandomMail()
         }
+
 
     }).mount('#app')
 
+
+
+
+
+
+    
+
+    
